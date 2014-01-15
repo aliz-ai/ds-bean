d42bc1bd00e0b46eb6d28542f445680d125a3f6e
gabor-farkas <gabor.farkas@doctusoft.com>
2014-01-15 15:39:54 +0100

    projects restructured

--- ds-bean/src/main/java/com/doctusoft/common/core/bean/Attributes.java
+++ ds-bean/src/main/java/com/doctusoft/common/core/bean/Attributes.java
@@ -4,12 +4,9 @@
 
 package com.doctusoft.common.core.bean;
 
-import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
-import com.doctusoft.common.core.bean.internal.AttributeImpl;
 import com.google.common.base.Function;
-import com.google.common.base.Strings;
 
 /**
  *
@@ -18,18 +15,6 @@ import com.google.common.base.Strings;
 public final class Attributes {
 
 	private Attributes() {}
-
-	/**
-	 * @deprecated this implementation relies on {@link AttributeImpl} that uses reflection. This
-	 *   solution has been replaced with inline code generation using lombok-ds.
-	 */
-	@Deprecated
-	public static <E, T> Attribute<E, T> of( final Class<E> parent, final String name, final Class<T> type ) {
-		checkNotNull( parent );
-		checkArgument( !Strings.isNullOrEmpty( name ) );
-
-		return new AttributeImpl<E, T>(parent, name, type);
-	}
 
 	public static <E, T> Function<E, T> functionOf( final Attribute<E, T> attribute ) {
 		return new AttributeFunction<E, T>( attribute );
